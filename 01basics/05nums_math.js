ae5b383a3d883d9cbeb48e95c6c39d1a05704a6f
sanchit474 <sanchitbajpai11@gmail.com>
2024-10-20 07:30:38 +0000

    basic 1 complete

---
+++ 01basics/05nums_math.js
@@ -0,0 +1,27 @@
+const num = 123
+// console.log(num);
+
+const balance = new Number(100) //explicitly define number 
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //indian format with ,
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));     //absolute value
+// console.log(Math.round(4.6));  // roundoff value 
+// console.log(Math.ceil(4.2));  //ceil value
+// console.log(Math.floor(4.9));  //foor value
+// console.log(Math.min(4, 3, 6, 8));   //minimum value
+// console.log(Math.max(4, 3, 6, 8));   //maximum value
+
+console.log(Math.random());  //random value 0-1
