a3a8247becb6ddafafa7e9a807786f8c14fdacaa
sanchit474 <sanchitbajpai11@gmail.com>
2024-10-20 07:36:30 +0000

    number and math

--- 01basics/05nums_math.js
+++ 01basics/05nums_math.js
@@ -22,6 +22,6 @@ const hundreds = 1000000
 // console.log(Math.ceil(4.2));  //ceil value
 // console.log(Math.floor(4.9));  //foor value
 // console.log(Math.min(4, 3, 6, 8));   //minimum value
-// console.log(Math.max(4, 3, 6, 8));   //maximum value
+console.log(Math.max(4, 3, 6, 8));   //maximum value
 
 console.log(Math.random());  //random value 0-1
